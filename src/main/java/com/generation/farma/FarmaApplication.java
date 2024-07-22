cbe977012bcb2b3891f662a1fd0237f721bcf123
Lucas Rodrigues <lucasrctt@gmail.com>
2024-07-22 10:05:51 -0300

    feat: adiciona configurações iniciais

---
+++ src/main/java/com/generation/farma/FarmaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.farma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaApplication.class, args);
+	}
+
+}
